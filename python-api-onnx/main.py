fb62eef374a982e2e54451f83f55be6ed1e59f77
victormacedo996 <victormacedo996@gmail.com>
2025-02-18 19:17:03 -0300

    feat: add observability to python-api-onnx

--- python-api-onnx/main.py
+++ python-api-onnx/main.py
@@ -4,10 +4,41 @@ import os
 from dto.response import HealthResponse, PredictResponse
 from dto.request import PredictRequest
 import onnxruntime as rt
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry import metrics, trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
 app = FastAPI()
 
 WEBSERVER_PORT = int(os.getenv("WEBSERVER_PORT", 5000))
+OTEL_SERVICE_NAME = os.getenv("OTEL_SERVICE_NAME", "poc-python-api-onnx")
+OTEL_COLLECTOR_OTLP_ENDPOINT = os.getenv("OTEL_COLLECTOR_OTLP_ENDPOINT", "otelcol:4317")
+
+otel_resource = Resource(attributes={
+    SERVICE_NAME: OTEL_SERVICE_NAME,
+})
+
+otlp_exporter = OTLPMetricExporter(endpoint=f"http://{OTEL_COLLECTOR_OTLP_ENDPOINT}")
+otlp_metric_reader = PeriodicExportingMetricReader(otlp_exporter)
+meter_provider = MeterProvider(resource=otel_resource, metric_readers=[otlp_metric_reader])
+metrics.set_meter_provider(meter_provider)
+meter = metrics.get_meter(__name__)
+meter_provider = meter_provider
+
+otlp_span_exporter = OTLPSpanExporter(endpoint=f"http://{OTEL_COLLECTOR_OTLP_ENDPOINT}",)
+span_processor = BatchSpanProcessor(otlp_span_exporter)
+trace_provider = TracerProvider(resource=otel_resource)
+trace_provider.add_span_processor(span_processor)
+trace.set_tracer_provider(tracer_provider=trace_provider)
+tracer = trace.get_tracer(__name__)
+tracer_provider = trace_provider
+
 
 PREDICTION_MAPPING = {
     0: "iris-setosa",
@@ -33,6 +64,7 @@ async def predict(model_input: PredictRequest) -> PredictResponse:
     prediction = onnx_predict(model_input.sepal_length, model_input.sepal_width, model_input.petal_length, model_input.petal_width)
     return PredictResponse(prediction=prediction)
 
+FastAPIInstrumentor.instrument_app(app, tracer_provider=trace_provider, meter_provider=meter_provider)
 
 if __name__ == '__main__':
     uvicorn.run(app, port=WEBSERVER_PORT, host="0.0.0.0")
