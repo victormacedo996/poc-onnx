d169d81a6195302df8111a08140c3f407f7f3fe1
Victor Macedo <victormacedo996@gmail.com>
2025-02-18 19:17:58 -0300

    Feat/observability (#6)
    * feat: start to add observability stack
    
    * refactor: update .gitignore
    
    * feat: add observabiity stack components
    
    * feat: add observability to go-api-onnx
    
    * feat: add observability to python-api-onnx
    
    * feat: add observability to python-api-pkl
    
    * feat: add observability stack to compose

--- go-api-onnx/internal/config/config.go
+++ go-api-onnx/internal/config/config.go
@@ -6,16 +6,20 @@ import (
 )
 
 type Config struct {
-	WEBSERVER_PORT  int
-	SHARED_LIB_PATH string
-	MODEL_PATH_ONNX string
+	WEBSERVER_PORT               int
+	SHARED_LIB_PATH              string
+	MODEL_PATH_ONNX              string
+	OTEL_COLLECTOR_OTLP_ENDPOINT string
+	OTEL_SERVICE_NAME            string
 }
 
 func NewConfig() *Config {
 	return &Config{
-		WEBSERVER_PORT:  parseEnvToInt("WEBSERVER_PORT", "5000"),
-		SHARED_LIB_PATH: getEnv("SHARED_LIB_PATH", ""),
-		MODEL_PATH_ONNX: getEnv("MODEL_PATH_ONNX", ""),
+		WEBSERVER_PORT:               parseEnvToInt("WEBSERVER_PORT", "5000"),
+		SHARED_LIB_PATH:              getEnv("SHARED_LIB_PATH", ""),
+		MODEL_PATH_ONNX:              getEnv("MODEL_PATH_ONNX", ""),
+		OTEL_COLLECTOR_OTLP_ENDPOINT: getEnv("OTEL_COLLECTOR_OTLP_ENDPOINT", "otelcol:4317"),
+		OTEL_SERVICE_NAME:            getEnv("OTEL_SERVICE_NAME", "go-api-onnx"),
 	}
 }
 
